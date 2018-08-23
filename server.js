e5e1cdeb8aaaad5da39aecccdfec26535589adbb
Jaroslav Vercinskij <verc@danskebank.lt>
2018-08-23 15:03:53 +0300

    Added status endpoint.

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+
 app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+  res.send(JSON.stringify({ Hello: 'World'}));
 });
+
 app.get('/name', function (req, res) {
   console.log('Request received to GET name. Headers: ', req.headers);
- res.send('go away');
+  res.send('go away');
 });
+
 app.post('/name', function (req, res) {
   console.log('Request received to POST name. Headers: ', req.headers);
- res.send('go away');
+  res.send('go away');
 });
+
+app.post('/status', function(req, res) {
+  console.log('Request received to POST status. Headers: ', req);
+  res.send('No.');
+});
+
 app.listen(port, function () {
  console.log('Example app listening on port !');
 });
