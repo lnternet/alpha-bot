e259eda5cce68646a406878a979497d097123e24
Jaroslav Vercinskij <verc@danskebank.lt>
2018-11-05 17:59:33 +0200

    commented out ode

--- server.js
+++ server.js
@@ -91,13 +91,13 @@ function getNumberOfPeople(room) {
   }
 
   let result = '0';
-  if(file != null) {
-    var result = fs.readFile('static/canteen.txt','utf8', function(error, content) {
-      console.log('CONTENT:', content);
-      if(content !== null)
-        result = content;
-    });
-  }
+  // if(file != null) {
+  //   var result = fs.readFile('static/canteen.txt','utf8', function(error, content) {
+  //     console.log('CONTENT:', content);
+  //     if(content !== null)
+  //       result = content;
+  //   });
+  // }
   return result;
 }
 
